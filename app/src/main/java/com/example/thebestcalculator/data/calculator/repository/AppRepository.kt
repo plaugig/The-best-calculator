5c79ac0f8ddf43d4ed26340abee937c628e6e0ab
George <george.d.kim@gmail.com>
2026-02-06 12:12:48 -0500

    Refactor

--- app/src/main/java/com/example/thebestcalculator/data/calculator/repository/AppRepository.kt
+++ app/src/main/java/com/example/thebestcalculator/data/calculator/repository/AppRepository.kt
@@ -8,31 +8,31 @@ import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class AppRepository @Inject constructor(
-    private val localAppDataSource: LocalAppDataSource
+	private val localAppDataSource: LocalAppDataSource
 ) {
-    fun getSaveInput(): Flow<AppData?> {
-        return localAppDataSource.observeInput().map { entity ->
-            entity?.let {
-                AppData(
-                    id = it.id,
-                    expression = it.expression,
-                    result = it.result
-                )
-            }
-        }
-    }
+	fun getSaveInput(): Flow<AppData?> {
+		return localAppDataSource.getInput().map { entity ->
+			entity?.let {
+				AppData(
+					id = it.id,
+					expression = it.expression,
+					result = it.result
+				)
+			}
+		}
+	}
 
-    suspend fun saveInput (data: AppData) {
-        val entity = AppEntity(
-            id = data.id,
-            expression = data.expression,
-            result = data.result
+	suspend fun saveInput(data: AppData) {
+		localAppDataSource.saveInput(
+            input = AppEntity(
+                id = data.id,
+                expression = data.expression,
+                result = data.result
+            )
         )
-        localAppDataSource.saveInput(entity)
-    }
+	}
 
-    suspend fun clear(){
-        localAppDataSource.clearInput()
-    }
-
+	suspend fun clear() {
+		localAppDataSource.clearInput()
+	}
 }
